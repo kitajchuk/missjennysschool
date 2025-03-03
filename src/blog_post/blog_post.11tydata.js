37cabcf201b5ecda121724fbf2ed8cbc8a55f136
kitajchuk <bk@kitajchuk.com>
2025-03-03 14:39:26 -0800

    blog rollio

--- src/blog_post/blog_post.11tydata.js
+++ src/blog_post/blog_post.11tydata.js
@@ -1,6 +1,21 @@
+import { asText } from "../../lib/filters/as_text.js";
 import { recursiveGetByType } from "../../lib/prismic.client.js";
 
 export default async function () {
-  const posts = await recursiveGetByType("blog_post");
-  return { posts };
+  const posts = await recursiveGetByType("blog_post", {
+    orderings: [
+      {
+        field: "my.blog_post.published_date",
+        direction: "desc",
+      },
+    ],
+  });
+  return {
+    posts,
+    eleventyComputed: {
+      title: (data) => asText(data.post.data.title),
+      related_posts: (data) =>
+        data.posts.filter((post) => post.uid !== data.post.uid).slice(0, 3),
+    },
+  };
 }
